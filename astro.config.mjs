e6057e0347ad7af3fd37e9a3161b84fd1af7b356
nblakey <nblakey.gd@gmail.com>
2024-07-29 20:25:58 -0400

    set up astro config site details

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://nblakey.github.io',
+  base: '/tf-logistics'
 });
